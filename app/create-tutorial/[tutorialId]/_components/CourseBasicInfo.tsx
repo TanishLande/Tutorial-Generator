c78fa3395388e8c57ffdf39595087195744d13e1
Tanish Lande <tanishlande10@gmail.com>
2024-09-21 14:12:25 +0530

    responsive image done

--- app/create-tutorial/[tutorialId]/_components/CourseBasicInfo.tsx
+++ app/create-tutorial/[tutorialId]/_components/CourseBasicInfo.tsx
@@ -36,30 +36,32 @@ const TutorialBasicInfo = ({ course }: TutorialBasicInfoProps) => {
   return (
     <div className='p-10 border rounded-xl shadow-sm mt-5'>
       <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
-        <div>
-          <h2 className='font-bold text-3xl flex gap-x-2 '>
-            {course.courseOutput.course.name} <EditCourseBsicInfo course={course} />
-          </h2>
-          <p className='text-sm text-gray-400 mb-5 sm:mb-0'>
-            {course.courseOutput.course.description}
-          </p>
-          <h2
-            className='font-medium mt-2 flex gap-2 items-center text-blue-900'
-          >
-           <TbCategoryPlus />{course.courseOutput.course.category}
-          </h2>
-          <Button
-            className='w-full mt-5'
-            variant='blue'
-          >
-            Start
-          </Button>
+      <div className="flex flex-col h-full">
+          <div>
+            <h2 className='font-bold text-3xl flex gap-x-2 '>
+              {course.courseOutput.course.name} <EditCourseBsicInfo course={course} />
+            </h2>
+            <p className='text-sm text-gray-400 mb-5 sm:mb-0'>
+              {course.courseOutput.course.description}
+            </p>
+            <h2 className='font-medium mt-2 flex gap-2 items-center text-blue-900'>
+              <TbCategoryPlus />{course.courseOutput.course.category}
+            </h2>
+          </div>
+          
+          {/* Button is placed in a div that will push it to the bottom */}
+          <div className="mt-auto">
+            <Button className='w-full mt-5' variant='blue'>
+              Start
+            </Button>
+          </div>
         </div>
+
         <div>
             <Image 
                 src='/images/placeholder.png'
                 alt="Image Placeholder"
-                className='w-full rounded-xl h-[250px] object-cover'
+                className='w-full rounded-xl h-[180px] object-cover md:h-[300px] lg:h-[350px]'
                 width={300}
                 height={300}
             />
